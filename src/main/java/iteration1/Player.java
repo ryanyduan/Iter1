56f09a5b30aa4aa5f6422beff8aebec0f9047105
Ryan <ryanyduan@gmail.com>
2018-10-22 14:00:26 -0400

    displayHand works

--- src/main/java/iteration1/Player.java
+++ src/main/java/iteration1/Player.java
@@ -2,15 +2,33 @@ package iteration1;
 
 import java.util.ArrayList;
 
-public class Player{
-
+public class Player extends Observer{
+	
+	private String playerType;
 	public ArrayList<Tile> Hand;
+	public ArrayList<ArrayList<Tile>> runs;
+	private ArrayList<ArrayList<Tile>> sets;
 
-	public Player() {
-		
+	public Player(String type, Table table) {
+		this.playerType = type;
 		this.Hand = new ArrayList<Tile>();
+		this.table = table;
 	}
 
+
 	
+	public String displayHand(){
+		String returnHand = "";
+		for (Tile t: this.Hand) {
+			returnHand += t.toString();
+		}
+		return returnHand;
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
