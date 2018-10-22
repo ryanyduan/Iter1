636c74092d49a514d30d4cb258d4f7bb34403db6
Ryan <ryanyduan@gmail.com>
2018-10-22 13:18:03 -0400

    initial test for making sure two tiles in hand are properly organized with comparable

---
+++ src/test/java/iteration1/TileTest.java
@@ -0,0 +1,25 @@
+package iteration1;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class TileTest extends TestCase {
+
+	public void testCompare2Tiles() {
+		Player player = new Player();
+		Tile B_one = new Tile('B', 1);
+		Tile B_two = new Tile('B',2);
+		player.Hand.add(B_two);
+		player.Hand.add(B_one);
+		player.Hand.shuffle();
+		
+		ArrayList<Tile> confirmationArray = new ArrayList<Tile>();
+		confirmationArray.add(B_two);
+		confirmationArray.add(B_one);
+		assertEquals(confirmationArray, player.Hand);
+	}
+
+}
