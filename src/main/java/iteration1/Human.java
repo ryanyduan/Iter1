2e4cd102ddf12480b00d2c201e67447696530237
Ryan <ryanyduan@gmail.com>
2018-10-23 19:54:03 -0400

    player gets list of possible options of tiles to play in a hashmap

--- src/main/java/iteration1/Human.java
+++ src/main/java/iteration1/Human.java
@@ -14,9 +14,37 @@ public class Human extends Player {
 
 	@Override
 	public void turn() {
-	
+		runs = this.findRuns();
+		sets = this.findSets();
+		turnOptions = new HashMap<String, ArrayList<Tile>>();
+		
+		System.out.println(runs.toString());
+		int counter = 0;
+		for (ArrayList<Tile> run: runs) {
+			turnOptions.put(counter, run);
+			counter++;
+		}
+		for (ArrayList<Tile> set: sets) {
+			turnOptions.put(counter, set);
+			counter++;
+		}
+		
+		System.out.println("Here are your options of melds to play");
+		printMap(turnOptions);
+		
+		
+		
+		
 	}
 	
+	public static void printMap(HashMap map) {
+	    Iterator it = map.entrySet().iterator();
+	    while (it.hasNext()) {
+	        HashMap.Entry pair = (HashMap.Entry)it.next();
+	        System.out.println(pair.getKey() + " = " + pair.getValue());
+	        it.remove(); // avoids a ConcurrentModificationException
+	    }
+	}
 	
 
 }
