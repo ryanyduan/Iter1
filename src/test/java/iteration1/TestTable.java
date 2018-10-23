9d8469a4a208204fe48a8fac21f68e63428d7bea
Ryan <ryanyduan@gmail.com>
2018-10-23 17:35:41 -0400

    displayBoard works for two melds

--- src/test/java/iteration1/TestTable.java
+++ src/test/java/iteration1/TestTable.java
@@ -16,6 +16,23 @@ public class TestTable extends TestCase {
 		assertEquals("{[B1 , B2 , B3 ]}", table.displayBoard());
 	}
 	
+	public void testDisplayTable2Melds() {
+		Table table = new Table();
+		newMeld.add(B1);
+		newMeld.add(B2);
+		newMeld.add(B3);
+		table.Board.add(newMeld);
+		newMeld2.add(B1);
+		newMeld2.add(B2);
+		newMeld2.add(B3);
+		table.Board.add(newMeld2);
+		
+		assertEquals("{[B1 , B2 , B3 ]}{[B1 , B2 , B3 ]}", table.displayBoard());
+		
+	}
+	
+	ArrayList<Tile> newMeld = new ArrayList<Tile>();
+	ArrayList<Tile> newMeld2 = new ArrayList<Tile>();
 	Tile B1 = new Tile('B',1);
 	Tile B2 = new Tile('B',2);
 	Tile B3 = new Tile('B',3);
