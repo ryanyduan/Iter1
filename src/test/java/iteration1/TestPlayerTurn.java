a5f26850aa7e205330da1dec8a9fdef415082015
Ryan <ryanyduan@gmail.com>
2018-10-23 20:00:06 -0400

    correctly shows player two possible options

--- src/test/java/iteration1/TestPlayerTurn.java
+++ src/test/java/iteration1/TestPlayerTurn.java
@@ -1,5 +1,7 @@
 package iteration1;
 
+import java.util.Collections;
+
 import junit.framework.TestCase;
 
 public class TestPlayerTurn extends TestCase {
@@ -16,7 +18,33 @@ public class TestPlayerTurn extends TestCase {
 		human.turn();
 	}
 	
+	public void testBasicTurn2Melds() {
+		Player human = new Human(table, "Human");
+		human.Hand.clear();
+		human.Hand.add(B1);
+		human.Hand.add(B2);
+		human.Hand.add(B3);
+		human.Hand.add(R4);
+		human.Hand.add(G4);
+		human.Hand.add(O4);
+		human.Hand.add(G12);
+		human.Hand.add(B4);
+		Collections.sort(human.Hand);
+		
+		human.turn();
+		
+		
+	}
+	
+	Table table = new Table();
 	Tile B1 = new Tile('B',1);
 	Tile B2 = new Tile('B',2);
 	Tile B3 = new Tile('B',3);
+	
+	Tile R4 = new Tile('R',4);
+	Tile G4 = new Tile('G',4);
+	Tile O4 = new Tile('O',4);
+	Tile B4 = new Tile('B',4);
+	
+	Tile G12 = new Tile('G',12);
 }
