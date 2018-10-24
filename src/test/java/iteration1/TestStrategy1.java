5c5368b7b8dc4993813eda19c45a598bc72822ee
Ryan <ryanyduan@gmail.com>
2018-10-24 17:24:48 -0400

    test for if strategy1 has turn

--- src/test/java/iteration1/TestStrategy1.java
+++ src/test/java/iteration1/TestStrategy1.java
@@ -20,11 +20,24 @@ public class TestStrategy1 extends TestCase {
 		
 	}
 	
+	public void testStrat1Has30() {
+		s1.Hand.clear();
+		s1.Hand.add(G11);
+		s1.Hand.add(B11);
+		s1.Hand.add(O11);
+		
+		assertTrue(s1.turn());
+	}
+	
 	Table table = new Table();
 	Player s1 = new Strategy1(table, "S1");
 	
 	Tile B1 = new Tile('B',1);
 	Tile B2 = new Tile('B',2);
 	Tile B3 = new Tile('B',3);
+	
+	Tile O11 = new Tile('O',11);
+	Tile G11 = new Tile('G',11);
+	Tile B11 = new Tile('B',11);
 
 }
