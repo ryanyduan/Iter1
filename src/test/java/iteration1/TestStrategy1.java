f4c12ed701aff59a511b3937661101b9172b940e
Ryan <ryanyduan@gmail.com>
2018-10-24 17:11:04 -0400

    initial test for making sure strategy1 doesn't play until they have 30

--- src/test/java/iteration1/TestStrategy1.java
+++ src/test/java/iteration1/TestStrategy1.java
@@ -9,10 +9,22 @@ public class TestStrategy1 extends TestCase {
 		Player s1 = new Strategy1(table, "S1");
 		s1.Hand.clear();
 		assertFalse(s1.turn());
+	}
+	
+	public void testStrat1No30() {
+		s1.Hand.clear();
+		s1.Hand.add(B1);
+		s1.Hand.add(B2);
+		s1.Hand.add(B3);
+		assertFalse(s1.turn());
 		
 	}
 	
 	Table table = new Table();
+	Player s1 = new Strategy1(table, "S1");
 	
+	Tile B1 = new Tile('B',1);
+	Tile B2 = new Tile('B',2);
+	Tile B3 = new Tile('B',3);
 
 }
